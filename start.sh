9a7ce90b6bb961a5c61c6dfaed35ca9ff6fd4235
Yashraj Chouhan <yashrajchouhan14@gmail.com>
2025-04-19 03:34:39 +0530

    again2

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
-#!/bin/sh
-./pocketbase serve --http=0.0.0.0:8090 --dir=/pb_data --migrations=/pb_migrations
+#!/bin/bash
+set -e
+cd "$(dirname "$0")"
+exec ./pocketbase serve --http=0.0.0.0:8090 --dir=/pb_data --migrations=/pb_migrations
