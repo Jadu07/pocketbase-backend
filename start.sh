b72a4464222dd3ca9b003cc96e88c4514b20f0e5
Yashraj Chouhan <yashrajchouhan14@gmail.com>
2025-04-19 03:31:45 +0530

    again fixture

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./pocketbase serve --http=0.0.0.0:8090 --dir=/app/pb_data --migrations=/app/pb_migrations
+./pocketbase serve --http=0.0.0.0:8090 --dir=/pb_data --migrations=/pb_migrations
